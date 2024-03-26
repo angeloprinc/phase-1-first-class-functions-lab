fca294bec755fc5b3404142a58bf6ce72ee72289
ANGELO PRINCE <angelo@angelo.com>
2024-03-26 08:44:16 +0300

    first class functions lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file!
+
+
+// TODO:- Declare the variable returnFirstTwoDrivers with const and assign an anonymous function to it.
+// TODO:- The assigned function should take one argument, an array containing the names of Scuber's drivers.
+const returnFirstTwoDrivers = function (namesArr){
+  // TODO:- Return a new array containing the first two drivers in the array.
+  return namesArr.slice(0, 2);
+};
+
+// ----------------------------------------
+
+// TODO:- Declare a variable with const that is assigned an anonymous function.
+// TODO:- The assigned function should take one argument, an array containing the names of Scuber's drivers.
+const returnLastTwoDrivers = function (namesArr){
+  // TODO:- Return a new array containing the last two drivers in the array.
+  return namesArr.slice(-2);
+};
